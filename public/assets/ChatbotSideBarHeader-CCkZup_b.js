be0496666e1e82692ff995f4856883d5199a27c6
tiyip <tiyip@redhat.com>
2026-08-26 12:57:47 -0400

    feat: rebuild for GitLab Pages demo deployment

---
+++ public/assets/ChatbotSideBarHeader-CCkZup_b.js
@@ -0,0 +1,12 @@
+import{dN as i}from"./patternfly-WslMbMBp.js";import{i as r,L as a,V as o}from"./ansible-chatbot-Bx-u4Mwx.js";import{u as s,d as t}from"./PlatformMain-8J19e7sJ.js";import"./monaco-editor-BV9BhtGa.js";import"./pfcharts-g8Sk7xaS.js";import"./monaco-yaml-D15VzaiI.js";import"./pfquickstarts--qnB4XQ8.js";import"./pftopology-D9kysgca.js";import"./index-Cj5tt3kE.js";const n=t.img`
+  height: 40px;
+  display: inline-block;
+  vertical-align: middle;
+`,d=t.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`,l=t.span`
+  display: inline-block;
+  vertical-align: middle;
+`,j=()=>{const{activeTheme:e}=s();return i.jsxs(d,{children:[i.jsx(n,{src:String(e==="dark"?r:a),alt:"Lightspeed Logo"}),i.jsx(l,{children:o()})]})};export{j as default};
