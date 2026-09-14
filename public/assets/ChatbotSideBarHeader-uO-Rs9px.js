f262813aa80cf66ddae58166d3298e52dd1b2c33
tiyip <tiyip@redhat.com>
2026-09-14 17:06:43 -0400

    Fix EDA prototype Pages deployment

---
+++ public/assets/ChatbotSideBarHeader-uO-Rs9px.js
@@ -0,0 +1,12 @@
+import{dN as i}from"./patternfly-CRKs2gbp.js";import{i as a,L as r,V as s}from"./ansible-chatbot-mjwijQdr.js";import{u as o,d as e}from"./PlatformMain-aXomoBgk.js";import"./monaco-editor-CliIDu1H.js";import"./pfcharts-BqC7r5pX.js";import"./monaco-yaml-D15VzaiI.js";import"./pfquickstarts-BnBGH8MH.js";import"./pftopology-CUGbZ7PH.js";const n=e.img`
+  height: 40px;
+  display: inline-block;
+  vertical-align: middle;
+`,d=e.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`,l=e.span`
+  display: inline-block;
+  vertical-align: middle;
+`,L=()=>{const{activeTheme:t}=o();return i.jsxs(d,{children:[i.jsx(n,{src:String(t==="dark"?a:r),alt:"Lightspeed Logo"}),i.jsx(l,{children:s()})]})};export{L as default};
