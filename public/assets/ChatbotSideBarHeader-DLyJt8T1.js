0bc536d2ac826820628ef8024cb0d5de5a63fd5b
tiyip <tiyip@redhat.com>
2026-09-14 16:58:22 -0400

    Publish EDA unbounded log growth demo

---
+++ public/assets/ChatbotSideBarHeader-DLyJt8T1.js
@@ -0,0 +1,12 @@
+import{j as i}from"./patternfly-B-6bcD11.js";import{i as a,L as s,V as r}from"./ansible-chatbot-CuaiL3ML.js";import{u as n,d as e}from"./PlatformMain-oHX3pA_W.js";import"./index-BXhtryhX.js";import"./eda-utils-PCDlL5BP.js";const o=e.img`
+  height: 40px;
+  display: inline-block;
+  vertical-align: middle;
+`,l=e.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`,d=e.span`
+  display: inline-block;
+  vertical-align: middle;
+`,h=()=>{const{activeTheme:t}=n();return i.jsxs(l,{children:[i.jsx(o,{src:String(t==="dark"?a:s),alt:"Lightspeed Logo"}),i.jsx(d,{children:r()})]})};export{h as default};
